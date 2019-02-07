a80eab62dfce56340137ef061eebe17c6a685216
jpclark6 <jpclark6@gmail.com>
2019-02-07 13:17:38 -0700

    removes pry

--- app/mailers/confirmation_notifier_mailer.rb
+++ app/mailers/confirmation_notifier_mailer.rb
@@ -2,7 +2,6 @@ class ConfirmationNotifierMailer < ApplicationMailer
   def confirm(user)
     @user = user
     confirm_code = SecureRandom.hex(15)
-    binding.pry
     @user.update(confirmation: confirm_code)
     mail(to: @user.email, subject: 'Confirm Email')
   end
