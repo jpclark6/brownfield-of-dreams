fd8d38329cfa148ecc0ddfaf39b1d07cff678da8
jpclark6 <jpclark6@gmail.com>
2019-02-08 10:42:01 -0700

    adds mailser

--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Friend, type: :model do
-  # describe 'relationships' do
-  #   it { should have_many :friendships }
-  #   it { should have_many(:users).through(:friendships) }
-  # end
   it 'exists' do
     user = create(:user)
     user_2 = create(:user)
